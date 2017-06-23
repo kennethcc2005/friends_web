6dd7ba188b6e5e21e2b0b16e07de354ebc2f1245
Zoesh <kennethcc2005@gmail.com>
2017-06-23 10:00:10 -0500

    updating...

--- src/index.js
+++ src/index.js
@@ -12,8 +12,3 @@ ReactDom.render(
   <MuiThemeProvider muiTheme={getMuiTheme()}>
     <Router history={browserHistory} routes={routes} />
   </MuiThemeProvider>, document.getElementById('root'));
-
-// ReactDom.render(
-//   <Router history={browserHistory} routes={routes} />,
-//   document.getElementById('root')
-// );
